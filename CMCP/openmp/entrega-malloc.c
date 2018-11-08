46699c2fde1aeedf147da7ec930d92b9713c8233
Javier Rodríguez <javierrd96@gmail.com>
2018-11-08 09:11:45 +0100

    cleaned source

--- CMCP/openmp/entrega-malloc.c
+++ CMCP/openmp/entrega-malloc.c
@@ -21,11 +21,8 @@ void add (int A[], int B[], int C[], int N) {
 
 		C[i] = sum;
 	}
-
-	if (carry) printf ("overflow in addition!\n");
 }
 
-/* B = n * A */
 void multiply_one_digit (int A[], int B[], int n, int N) {
 	int	i, carry;
 
@@ -37,42 +34,28 @@ void multiply_one_digit (int A[], int B[], int n, int N) {
 
 		B[i] += carry;
 
-		if (B[i] >= 10) {
-
+		if (B[i] >= 10) {      
 			carry = B[i] / 10;
 			B[i] %= 10;
 		} else
-
 			carry = 0;
 	}
-	if (carry) printf ("overflow in multiplication!\n");
 }
 
-/* "multiplies" a number by BASEn */
 void shift_left (int A[], int n, int N) {
 	int	i;
 
 	for (i=N-1; i>=n; i--) A[i] = A[i-n];
-
 	while (i >= 0) A[i--] = 0;
 }
 
 
-/* C = A * B */
 void multiply (int A[], int B[], int C[], int N) {
 	int	i, j, P[N];
 
 	for (i=0; i<N; i++) {
-		/* multiply B by digit A[i] */
-
 		multiply_one_digit (B, P, A[i], N);
-
-		/* shift the partial product left i bytes */
-
 		shift_left (P, i, N);
-
-		/* add result to the running sum */
-
 		add (C, P, C, N);
 	}
 }
@@ -80,13 +63,11 @@ void multiply (int A[], int B[], int C[], int N) {
 
 main(int argc, char**argv)
 {
+    // DECLARACION DE VARIABLES
+
     double t1,t2,tucpu,tscpu;
-    //printf("%s\n", argv[1]);
     int len1 = strlen(argv[1]);
-    //printf("%d\n",len1);
-    //printf("%s\n", argv[2]);
     int len2 = strlen(argv[2]);
-    //printf("%d\n",len2);
     int N = len1+len2;
     int A[N], B[N], C[N];
 
@@ -95,6 +76,8 @@ main(int argc, char**argv)
         B[i] = 0;
         C[i] = 0;
     }
+
+    // RELLENADO DE MATRICES
 
     char k[len1];
     strcpy(k, argv[1]);
@@ -131,34 +114,22 @@ main(int argc, char**argv)
     printf(" ------- \n");
     printf("Tiempo %f segundos \n",(float) (t2-t1));
     printf(" ------- \n");
+
+
     // PARALELO
 
-    
-
-    int E[N];
-    for(int i=0;i < N; i++)
-        E[i] = 0;
-
-    //multiply
     printf("---PARALELO---\n");
 
-    printf("A  [ ");
-    for(int loop = N-1; loop >= 0; loop--)
-        printf("%d ", A[loop]);
-    printf("]\n");
-
-    printf("B  [ ");
-    for(int loop = N-1; loop >= 0; loop--)
-        printf("%d ", B[loop]);
-    //printf("]\nC  [ ");
-    printf("]\n");
-
-    //omp_set_dynamic(0); 
     omp_set_num_threads(4);
 
     int D[4*N];
     int n, i, carry,j,sum, P[N], tid, nthreads;
+    int E[N];
+    for(int i=0;i < N; i++)
+        E[i] = 0;
+    
     ctimer(&t1,&tucpu,&tscpu);
+
     #pragma omp parallel shared (B,A) private(i,n, carry, j, sum, P, tid)
     {
         nthreads = omp_get_num_threads();
@@ -167,26 +138,17 @@ main(int argc, char**argv)
         }
         #pragma omp barrier
         tid = omp_get_thread_num();
-        //printf("soy el thread %u de %u \n", tid, nthreads);
+
         for (i=tid; i<len1; i=i+nthreads) {
             
             n = A[i];
 
-            //printf("\nthread %d i %d n %d\n",tid,i,n);
-            if(tid==0){
-                //printf("\nthread %d i %d n %d\n",tid,i,n);
-                /*printf("Pbefore  [ ");
-                for(int loop = N-1; loop >= 0; loop--)
-                    printf("%d ", P[loop]);
-                printf("]\n");*/
-            }
             carry = 0;
 
             for (j=0; j<N; j++) {
 
                 P[j] = n * B[j];
-                //if (tid==0)
-                    //printf("\nPJ %d n %d BJ %d\n",P[j],n,B[j]);
+
                 P[j] += carry;
 
                 if (P[j] >= 10) {
@@ -195,29 +157,13 @@ main(int argc, char**argv)
                 } else
                     carry = 0;
             }
-            if (carry) printf ("overflow in multiplication!\n");
 
-            /* shift the partial product left i bytes */
-            /*if(tid==0){
-                //printf("\nthread %d i %d n %d\n",tid,i,n);
-                printf("PSH0  [ ");
-                for(int loop = N-1; loop >= 0; loop--)
-                    printf("%d ", P[loop]);
-                printf("]\n");
-            }*/
+            // SHIFT
+
             for (j=N-1; j>=i; j--) P[j] = P[j-i];
-
             while (j >= 0) P[j--] = 0;
 
-            /* add result to the running sum */
-            /*if(tid==0){
-                //printf("\nthread %d i %d n %d\n",tid,i,n);
-                printf("P0  [ ");
-                for(int loop = N-1; loop >= 0; loop--)
-                    printf("%d ", P[loop]);
-                printf("]\n");
-            }*/
-            //int sum;
+            // SUMA Y ACUMULACION EN D
 
             carry = 0;
             sum = 0;
@@ -234,15 +180,13 @@ main(int argc, char**argv)
 
                 D[tid*N+j] = sum;
             }
-
-            if (carry) printf ("overflow in addition!\n");
         }
+
         #pragma omp barrier
+
+        // TRANSFERENCIA A E SUMANDO PARCIALES
+
         if(tid==0){
-            /*printf("D  [ ");
-            for(int loop = N*nthreads-1; loop >= 0; loop--)
-                printf("%d ", D[loop]);
-            printf("]\n");*/
 
             for(int k=0; k<nthreads;k++){
 
@@ -261,24 +205,28 @@ main(int argc, char**argv)
 
                     E[j] = sum;
                 }
-                if (carry) printf ("overflow in addition!\n");
             }
-
-            printf("C  [ ");
-            for(int loop = N-1; loop >= 0; loop--)
-                printf("%d ", E[loop]);
-            printf("]\n");
-
-        }
-        
+        }       
     }
+
     ctimer(&t2,&tucpu,&tscpu);
+
+    printf("A  [ ");
+    for(int loop = N-1; loop >= 0; loop--)
+        printf("%d ", A[loop]);
+    printf("]\n");
+
+    printf("B  [ ");
+    for(int loop = N-1; loop >= 0; loop--)
+        printf("%d ", B[loop]);
+    printf("]\n");
+
+    printf("C  [ ");
+    for(int loop = N-1; loop >= 0; loop--)
+        printf("%d ", E[loop]);
+    printf("]\n");
 
     printf(" ------- \n");
     printf("Tiempo %f segundos \n",(float) (t2-t1));
     printf(" ------- \n");
-    /*for(int loop = N-1; loop >= 0; loop--)
-        printf("%d ", C[loop]);
-    printf("]\n");*/
-
 }
