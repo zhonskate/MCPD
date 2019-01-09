1adb237aa76f36755ac6d1342d10bc23cc18f351
Javier Rodríguez <jrodriguez@iti.es>
2019-01-09 14:14:23 +0100

    09-01

--- CC/2018-11-05/FaaS/invoke.sh
+++ CC/2018-11-05/FaaS/invoke.sh
@@ -1,5 +1,5 @@
 max=100
 for i in `seq 2 $max`
 do
-    curl --header "Content-Type: application/json" --request PUT --data '{"a":10,"b":2}' http://localhost:3000/invokefunction/3266a184aeec777fac39d6e7b13cadeec71d4b894a4d4707191f2458b8b2af84
+    curl --header "Content-Type: application/json" --request PUT --data '{"a":10,"b":2}' http://localhost:3333/invokefunction/3266a184aeec777fac39d6e7b13cadeec71d4b894a4d4707191f2458b8b2af84
 done
