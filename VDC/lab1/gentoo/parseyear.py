17390c99b4fd5e3e4b8931c5aa70cb06b9b0f7ba
Javier Rodríguez <javierrd96@gmail.com>
2019-03-15 01:06:21 +0100

    finished prac1

---
+++ VDC/lab1/gentoo/parseyear.py
@@ -0,0 +1,30 @@
+f= open("data.txt","r")
+
+lines =f.readlines()
+# print(lines)
+currStation = ''
+currYear = -1
+currCount = 0
+currAmount = 0
+for i in range(len(lines)):
+    lines[i]=lines[i].split('\n')[0]
+    lineArr = lines[i].split('\t')
+    #print(lineArr)
+    #if currCount > 1:
+    #    print('count ' + str(currCount))
+    #    print('amount ' + str(currAmount))
+    #print(lineArr[0] + '  ' + currStation)
+    #print(lineArr[2] + '  ' + str(currYear))
+    if lineArr[2] == currYear:
+        currCount += 1
+        currAmount += int(lineArr[3])
+    else:
+        if i > 0:
+           print (str(currYear) + '\t' + str(currAmount/currCount))
+        currStation = lineArr[0]
+        currYear = lineArr[2]
+        currCount = 1
+        currAmount = int(lineArr[3])
+
+
+f.close() 
