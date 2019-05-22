460e57f3968311ce01e6a701fc3ccb1204913bc1
Javier Rodríguez <javierrd96@gmail.com>
2019-05-23 00:23:53 +0200

    23-05

--- CBD/3-pyspark/programs.py
+++ CBD/3-pyspark/programs.py
@@ -10,4 +10,13 @@ sc.textFile("/datasets/purchases/purchases.txt").map(lambda s: s.split("\t")).ma
 
 # 2 Número total de accesos al recurso "/assets/img/home-logo.png”
 
+sc.textFile("/datasets/accesslog/access_log").map(lambda s: s.split(" ")).map(lambda x: (x[6],1)).filter(lambda rec: rec[0]=="/assets/img/home-logo.png").count()
 
+
+# 3 Número total de accesos desde la misma dirección IP: 10.223.157.186
+
+sc.textFile("/datasets/accesslog/access_log").map(lambda s: s.split(" ")).map(lambda rec: (rec[0],1)).filter(lambda rec: rec[0]=="10.223.157.186").count()
+
+# 4 Recurso web con mayor número de accesos
+
+sc.textFile("/datasets/accesslog/access_log").map(lambda s: s.split(" ")).map(lambda rec: (rec[6],1)).reduceByKey(lambda x,y: x+y).max(key=lambda x: x[1])
---
+++ CBD/deliverable/activities/4_2_adelie_per_country/mapper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# Format of each line is:
+
+# site_name, site_id, cammlr_region, longitude, latitude, common_name, day, month, year, season_starting, penguin_count, accuracy, count_type, vantage, reference
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split(",")
+    if len(data) == 15:
+        site_name, site_id, cammlr_region, longitude, latitude, common_name, day, month, year, season_starting, penguin_count, accuracy, count_type, vantage, reference = data
+        if site_id == 'site_id':
+            continue
+        if common_name == 'ad{\\\'e}lie penguin': 
+            if float(longitude) >= -74.0 and float(longitude) <= -25.0:
+                print "{0}\t{1}".format('Argentina', penguin_count)
+	    if float(longitude) >= 142.2 and float(longitude) <= 160.0:
+                print "{0}\t{1}".format('Australia', penguin_count)
+            if float(longitude) >= 44.38 and float(longitude) <= 136.11:
+                print "{0}\t{1}".format('Australia', penguin_count)
+            if float(longitude) >= -90.0 and float(longitude) <= -53.0:
+                print "{0}\t{1}".format('Chile', penguin_count)
+            if float(longitude) >= 136.11 and float(longitude) <= 142.2:
+                print "{0}\t{1}".format('France', penguin_count)
+            if float(longitude) >= 160.0 or float(longitude) <= -150.0:
+                print "{0}\t{1}".format('New Zealand', penguin_count)
+            if float(longitude) >= -20.0 and float(longitude) <= 44.38:
+                print "{0}\t{1}".format('Norway', penguin_count)
+            if float(longitude) >= -80.0 and float(longitude) <= -20.0:
+                print "{0}\t{1}".format('United Kingdom', penguin_count)
